236034de45ceebd56e4220657e7a7bdc7b45b866
Daniel Robbins <drobbins@funtoo.org>
2020-04-19 14:49:31 -0600

    updates
    merged:
    
      flora: 00d3940c79b13cbc2ff4185aa82a4ee47f4a3ff8
      faustoo: 738e8c4ddf70452d513bb65a7cd52b41db675a69
      fusion809: 9c068da69db5ef69fccf67af46e2799503f55167
      deadbeef: 53e0a41db6e7193bd83ef117f8055457f0990cb7
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 1e3438ef7e43941ac0ffc6514e1d6748ebcb3ab5

---
+++ dev-vcs/git/autogen.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import json
+
+async def generate(hub, **pkginfo):
+
+	json_data = await hub.pkgtools.fetch.get_page("https://api.github.com/repos/git/git/tags")
+	json_list = json.loads(json_data)
+	for tag in json_list:
+		v = tag['name'].lstrip('v')
+
+		# Sanity checks:
+		#
+		# 1) Version 2.something.
+		# 2) At least minor version 26 (2.26+) to skip LTS releases
+		# 3) No release candidates.
+
+		vsplit = v.split('.')
+		if len(vsplit) != 3:
+			continue
+		if vsplit[0] != '2':
+			continue
+		try:
+			minor = int(vsplit[1])
+		except:
+			continue
+		if minor < 26:
+			continue
+		if "rc-" in v:
+			continue
+		version = v
+		break
+
+	kernel_org='https://www.kernel.org/pub/software/scm/git'
+	artifacts=[
+		hub.pkgtools.ebuild.Artifact(url=f'{kernel_org}/git-{version}.tar.xz'),
+		hub.pkgtools.ebuild.Artifact(url=f'{kernel_org}/git-manpages-{version}.tar.xz')
+	]
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=version,
+		artifacts=artifacts
+	)
+	ebuild.push()
+
+# vim: ts=4 sw=4 noet
