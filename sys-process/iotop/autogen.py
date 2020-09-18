94975327022cc435a1a55d3e9eef0d1b34077dc6
Daniel Robbins <drobbins@funtoo.org>
2020-09-18 13:14:03 -0600

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 2ce2518aaa729902a62c6fadf3dd8f99e2922e0c

--- sys-process/iotop/autogen.py
+++ sys-process/iotop/autogen.py
@@ -3,26 +3,24 @@
 import xmltodict
 import datetime
 
+
 async def generate(hub, **pkginfo):
 	page = await hub.pkgtools.fetch.get_page(f"https://repo.or.cz/iotop.git/atom")
 	xml_data = xmltodict.parse(page)
-	for datums in xml_data['feed']['entry']:
-		if 'id' not in datums:
+	for datums in xml_data["feed"]["entry"]:
+		if "id" not in datums:
 			continue
-		if not datums['id'].startswith('https://repo.or.cz/iotop.git/commitdiff/'):
+		if not datums["id"].startswith("https://repo.or.cz/iotop.git/commitdiff/"):
 			continue
-		sha1 = datums['id'].split('/')[-1]
-		datestamp = datums['published'].split('T')[0]
+		sha1 = datums["id"].split("/")[-1]
+		datestamp = datums["published"].split("T")[0]
 		break
 	url = f"https://repo.or.cz/iotop.git/snapshot/{sha1}.tar.gz"
-	version = datestamp.replace('-','.')
+	version = datestamp.replace("-", ".")
 	final_name = f"iotop-{version}.tar.gz"
 	# the latest tag is the first dict in the returned list of tag dicts
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo,
-		sha1=sha1,
-		version=version,
-		artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
+		**pkginfo, sha1=sha1, version=version, artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
 	)
 	ebuild.push()
 
