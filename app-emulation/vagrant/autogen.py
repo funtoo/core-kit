da1c813ecbed4a7f4a28b3c32cff5d5a566da57f
Daniel Robbins <drobbins@funtoo.org>
2020-12-14 11:05:39 -0700

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 95ea58c66b5308811b94bb0edb3ee6201ac361b0

--- app-emulation/vagrant/autogen.py
+++ app-emulation/vagrant/autogen.py
@@ -7,10 +7,14 @@ from re import match
 async def generate(hub, **pkginfo):
 	GITHUB_USER = "hashicorp"
 	GITHUB_REPO = "vagrant"
-	json_data = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/{GITHUB_USER}/{GITHUB_REPO}/tags")
-	json_list = json.loads(json_data)
-	GITHUB_TAG = json_list[0]["name"]
-	version = GITHUB_TAG.lstrip("v")
+	# json_data = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/{GITHUB_USER}/{GITHUB_REPO}/tags")
+	# json_list = json.loads(json_data)
+	# GITHUB_TAG = json_list[0]["name"]
+	# version = GITHUB_TAG.lstrip("v")
+
+	# This is a workaround for FL-7722 to lock the version to work with our current vagrant dependencies:
+	version = "2.2.10"
+	GITHUB_TAG = "v" + version
 	url = f"https://github.com/{GITHUB_USER}/{GITHUB_REPO}/archive/{GITHUB_TAG}.tar.gz"
 	final_name = f"{GITHUB_REPO}-{version}.tar.gz"
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
