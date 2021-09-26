a5327383e99d06d075a0f4bb717ef227bb703548
Daniel Robbins <drobbins@funtoo.org>
2021-09-26 12:07:21 -0600

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: d0e3affad25959be644955882c2beff42205144d

---
+++ sys-apps/bolt/autogen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import json
+
+async def generate(hub, **pkginfo):
+	gitlab_id = 32
+	json_data = await hub.pkgtools.fetch.get_page(
+		f"https://gitlab.freedesktop.org/api/v4/projects/{gitlab_id}/releases",
+		is_json=True
+	)
+	release = json_data[0]
+	url = list(filter(lambda x: x["format"] == "tar.bz2", release["assets"]["sources"]))[0]["url"]
+	version = pkginfo['version'] = release["name"]
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=url)],
+	)
+	ebuild.push()
+
+
+# vim: ts=4 sw=4 noet
