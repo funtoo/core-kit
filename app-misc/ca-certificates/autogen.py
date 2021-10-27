c1b37505e50f8ac162bf20fa8f1acecbcd5a6162
Daniel Robbins <drobbins@funtoo.org>
2021-10-26 23:01:33 -0600

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: b9bed63cd85e028f08c61f6f4f36f78369f6932c

---
+++ app-misc/ca-certificates/autogen.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import re
+import urllib.parse
+
+async def generate(hub, **pkginfo):
+
+	base_url=f"https://ftp.debian.org/debian/pool/main/c/ca-certificates/"
+
+	versions = await hub.pkgtools.pages.iter_links(
+		base_url=base_url,
+		match_fn=lambda x: re.match(r"^ca-certificates_([0-9]+)_all\.deb$", x),
+		fixup_fn=lambda x: x.groups()[0]
+	)
+	version = hub.pkgtools.pages.latest(versions)
+	url = urllib.parse.urljoin(base_url, f"ca-certificates_{version}_all.deb")
+	if version is None:
+		print("Could not find a ca-certificates version.")
+		return
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=version,
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=url)]
+	)
+
+	ebuild.push()
+
+# vim: ts=4 sw=4 noet
+
