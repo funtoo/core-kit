a239c8168e4dc139197da2148aeb91d823351723
Daniel Robbins <drobbins@funtoo.org>
2022-03-09 19:07:21 -0700

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: b5754d347f1f8e4a317eb3fec81eb551f03fc36c

---
+++ sys-apps/util-linux/autogen.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+
+
+async def generate(hub, **pkginfo):
+	python_compat="python3+"
+	tags_url = f"https://git.kernel.org/pub/scm/utils/util-linux/util-linux.git/refs/tags/"
+	html_data = await hub.pkgtools.fetch.get_page(tags_url)
+	soup = BeautifulSoup(html_data, "html.parser")
+	best_archive = None
+	for link in soup.find_all("a"):
+		href = link.get("href")
+		if "-rc" in href:
+			continue
+		if href.endswith(".tar.gz"):
+			best_archive = href
+			break
+	version = best_archive.split(".tar")[0].split("-")[-1]
+	shortver = ".".join(version.split(".")[0:2])
+	url = f"https://www.kernel.org/pub/linux/utils/util-linux/v{shortver}/util-linux-{version}.tar.xz"
+	artifact = hub.pkgtools.ebuild.Artifact(url=url)
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo, version=version, python_compat=python_compat, artifacts=[artifact],
+	)
+	ebuild.push()
+
+
+# vim: ts=4 sw=4 noet
