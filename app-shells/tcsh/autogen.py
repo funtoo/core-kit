7a18eb6292127e959df6313c1a62f92a11e9774a
Daniel Robbins <drobbins@funtoo.org>
2022-08-31 09:07:44 -0600

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: 973870b46190bb3699e256cb522b5fdfb7695bf5

---
+++ app-shells/tcsh/autogen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from packaging import version
+from bs4 import BeautifulSoup
+
+async def generate(hub, **pkginfo):
+	url = "https://astron.com/pub/tcsh/"
+	html_data = await hub.pkgtools.fetch.get_page(url)
+	soup = BeautifulSoup(html_data, "html.parser")
+	archives = {}
+	for link in soup.find_all("a"):
+		href = link.get("href")
+		if href is not None and href.endswith(".tar.gz"):
+			ver = href.split(".tar")[0].split("/")[-1].replace("tcsh-","")
+			if ver.upper().isupper():
+				continue
+			archives.update({ver:href})
+	latest_version = sorted(archives, key=lambda x: version.parse(x)).pop()
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=latest_version,
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=url+archives[latest_version])],
+	)
+	ebuild.push()
+
+# vim: ts=4 sw=4 noet
--- net-misc/curl/curl-7.85.0.ebuild
+++ net-misc/curl/curl-7.85.0.ebuild
@@ -6,7 +6,7 @@ inherit autotools eutils prefix multilib-minimal
 
 DESCRIPTION="A Client that groks URLs"
 HOMEPAGE="https://curl.haxx.se/"
-SRC_URI="https://api.github.com/repos/curl/curl/tarball/curl-7_84_0 -> curl-7.84.0.tar.gz" 
+SRC_URI="https://api.github.com/repos/curl/curl/tarball/curl-7_85_0 -> curl-7.85.0.tar.gz" 
 
 LICENSE="curl"
 SLOT="0"
@@ -91,7 +91,7 @@ MULTILIB_CHOST_TOOLS=(
 
 src_unpack() {
     default
-    mv "curl-curl-8e81b9a" "${S}" || die
+    mv "curl-curl-742b04e" "${S}" || die
 }
 
 src_prepare() {
