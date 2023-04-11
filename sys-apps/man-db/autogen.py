463f72255c2824dfb68dc21b8948f338f9aa0008
Daniel Robbins <drobbins@funtoo.org>
2023-04-11 00:06:09 +0000

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: b1a10470379ed886b4578f8b5cf182667ce8f17f

---
+++ sys-apps/man-db/autogen.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from bs4 import BeautifulSoup
+from packaging.version import Version
+import re
+
+regex = r'(\d+(?:[\.-]\d+)+)'
+
+async def generate(hub, **pkginfo):
+    name = pkginfo["name"]
+    download_url = f"https://download.savannah.nongnu.org/releases/{name}/"
+    html = await hub.pkgtools.fetch.get_page(download_url)
+    soup = BeautifulSoup(html, 'html.parser').find_all("a", href=True)
+
+    releases = [a for a in soup if name in a.contents[0] and a.contents[0].endswith('.xz')]
+    latest = max([(
+            Version(re.findall(regex, a.contents[0])[0]),
+            a.get('href'))
+        for a in releases if re.findall(regex, a.contents[0])
+    ])
+
+    artifact = hub.pkgtools.ebuild.Artifact(url=download_url + latest[1])
+
+    ebuild = hub.pkgtools.ebuild.BreezyBuild(
+        **pkginfo,
+        version=latest[0],
+        artifacts=[artifact],
+    )
+    ebuild.push()
+
