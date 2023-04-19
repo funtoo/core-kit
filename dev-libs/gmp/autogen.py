5cb97540bfa9e43e56d1c3e81add3710a9395da4
Daniel Robbins <drobbins@funtoo.org>
2023-04-19 17:09:03 +0000

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: efe40fd7193b7bbab81dd6935c82832a29174656

---
+++ dev-libs/gmp/autogen.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from bs4 import BeautifulSoup
+from packaging.version import Version
+import re
+
+regex = r'(\d+(?:[\.-]\d+)+)'
+
+async def generate(hub, **pkginfo):
+    name = pkginfo["name"]
+    base_url = f"https://gmplib.org"
+    download_url = f"{base_url}/download/{name}/"
+    html = await hub.pkgtools.fetch.get_page(download_url)
+    soup = BeautifulSoup(html, 'html.parser').find_all("a", href=True)
+
+    artifacts = {}
+
+    releases = [a for a in soup if name in a.contents[0] and a.contents[0].endswith('.xz')]
+    latest = max([(
+            Version(re.findall(regex, a.contents[0])[0]),
+            a.get('href'))
+        for a in releases if re.findall(regex, a.contents[0])
+    ])
+    version = latest[0]
+
+    artifacts[''] =  hub.pkgtools.ebuild.Artifact(url=download_url + latest[1])
+    artifacts['doc'] = hub.pkgtools.ebuild.Artifact(url=f"{base_url}/{name}-man-{version}.pdf")
+
+    ebuild = hub.pkgtools.ebuild.BreezyBuild(
+        **pkginfo,
+        version=latest[0],
+        artifacts=artifacts,
+    )
+    ebuild.push()
+
