870633ecd2e6173d8b10a523520cbbfad457eba9
Daniel Robbins <drobbins@funtoo.org>
2023-05-13 01:39:05 +0000

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: 78efb8533c86f407e293eb32854440e006c8f352

--- app-emulation/spice/generator.py
+++ app-emulation/spice/generator.py
@@ -6,16 +6,22 @@ async def generate(hub, **pkginfo):
     repo = pkginfo["name"]
     releases_url = f"{pkginfo['gitlab']['url']}/api/v4/projects/{pkginfo['gitlab']['project_id']}/releases"
     releases = await hub.pkgtools.fetch.get_page(releases_url, is_json=True)
+    prefix = pkginfo['gitlab']['prefix']
     if "version" not in pkginfo or pkginfo["version"] == "latest":
         for release in releases:
+            if not release['tag_name'].startswith(prefix):
+                continue
             if release["upcoming_release"]:
                 continue
-            pkginfo["version"] = release['tag_name'].lstrip("v")
+            pkginfo["version"] = release['tag_name'][len(prefix):]
             selected_release = release
             break
     else:
         for release in releases:
-            if release['tag_name'].lstrip("v") == pkginfo["version"]:
+            if not release['tag_name'].startswith(prefix):
+                continue
+            version = release['tag_name'][len(prefix):]
+            if version == pkginfo["version"]:
                 selected_release = release
                 break
 
