3f3f269b65a0e874d64bcca9f91ff7fdc2b81d1b
Daniel Robbins <drobbins@funtoo.org>
2023-07-30 22:47:17 +0000

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: c5acb5988b663ebb6defab0511a96f36d52aa2f5

--- dev-libs/gmp/autogen.py
+++ dev-libs/gmp/autogen.py
@@ -6,29 +6,29 @@ import re
 regex = r'(\d+(?:[\.-]\d+)+)'
 
 async def generate(hub, **pkginfo):
-    name = pkginfo["name"]
-    base_url = f"https://gmplib.org"
-    download_url = f"{base_url}/download/{name}/"
-    html = await hub.pkgtools.fetch.get_page(download_url)
-    soup = BeautifulSoup(html, 'html.parser').find_all("a", href=True)
+	name = pkginfo["name"]
+	base_url = f"https://gmplib.org"
+	download_url = f"{base_url}/download/{name}/"
+	html = await hub.pkgtools.fetch.get_page(download_url)
+	soup = BeautifulSoup(html, 'html.parser').find_all("a", href=True)
 
-    artifacts = {}
+	artifacts = {}
 
-    releases = [a for a in soup if name in a.contents[0] and a.contents[0].endswith('.xz')]
-    latest = max([(
-            Version(re.findall(regex, a.contents[0])[0]),
-            a.get('href'))
-        for a in releases if re.findall(regex, a.contents[0])
-    ])
-    version = latest[0]
+	releases = [a for a in soup if name in a.contents[0] and a.contents[0].endswith('.xz')]
+	latest = max([(
+			Version(re.findall(regex, a.contents[0])[0]),
+			a.get('href'))
+		for a in releases if re.findall(regex, a.contents[0])
+	])
+	version = latest[0]
 
-    artifacts[''] =  hub.pkgtools.ebuild.Artifact(url=download_url + latest[1])
-    artifacts['doc'] = hub.pkgtools.ebuild.Artifact(url=f"{base_url}/{name}-man-{version}.pdf")
+	artifacts = [ hub.pkgtools.ebuild.Artifact(url=download_url + latest[1]) ]
 
-    ebuild = hub.pkgtools.ebuild.BreezyBuild(
-        **pkginfo,
-        version=latest[0],
-        artifacts=artifacts,
-    )
-    ebuild.push()
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=latest[0],
+		artifacts=artifacts,
+	)
+	ebuild.push()
 
+# vim: ts=4 sw=4 noet
