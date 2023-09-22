6cda8746abe2d5a24b94e0dc239a493be5ba850b
Daniel Robbins <drobbins@funtoo.org>
2023-09-22 23:49:32 +0000

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: 02bc80ae3fda8996fd6f8a2ce7d04fab97135be7

--- app-arch/rpm/autogen.py
+++ app-arch/rpm/autogen.py
@@ -5,33 +5,39 @@ from packaging.version import Version
 import re
 
 regex = r'(\d+(?:\.\d+)+)'
+target_version = Version('4.18.1')
+target_major = Version('4.18')
 
 async def generate(hub, **pkginfo):
-    base_url = "http://ftp.rpm.org/releases/"
-    html = await hub.pkgtools.fetch.get_page(base_url)
-    soup = BeautifulSoup(html, 'html.parser').find_all('a', href=True)
+	base_url = "http://ftp.rpm.org/releases/"
+	html = await hub.pkgtools.fetch.get_page(base_url)
+	soup = BeautifulSoup(html, 'html.parser').find_all('a', href=True)
+	for a in soup:
+		if not a.get('href').startswith(pkginfo['name']):
+			continue
+		found_version = Version(re.findall(regex, a.get('href'))[0])
+		if str(found_version) == str(target_major):
+			download_url = base_url + a.get('href')
+			break
 
-    newest = max([(
-            Version(re.findall(regex, a.get('href'))[0]),
-            a.get('href')
-        )
-        for a in soup if a.get('href').startswith(pkginfo['name'])
-    ])
+	html = await hub.pkgtools.fetch.get_page(download_url)
+	soup = BeautifulSoup(html, 'html.parser').find_all('a', href=True)
+	url = None
+	for a in soup:
+		if '.tar.' not in a.get('href'):
+			continue
+		found_version = Version(re.findall(regex, a.get('href'))[0])
+		if str(found_version) == str(target_version):
+			pkginfo['version'] = str(found_version)
+			url = f"{download_url}{a.get('href')}"
+			break
+	if url is None:
+		raise ValueError("Couldn't find suitable version.")
+	artifact = hub.pkgtools.ebuild.Artifact(url=url)
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		artifacts=[artifact],
+	)
+	ebuild.push()
 
-    download_url = base_url + newest[1]
-    html = await hub.pkgtools.fetch.get_page(download_url)
-    soup = BeautifulSoup(html, 'html.parser').find_all('a', href=True)
-
-    latest = max([(
-            Version(re.findall(regex, a.get('href'))[0]),
-            a.get('href')
-        ) for a in soup if '.tar.' in a.get('href')
-    ])
-
-    artifact = hub.pkgtools.ebuild.Artifact(url=f"{download_url}{latest[1]}")
-    ebuild = hub.pkgtools.ebuild.BreezyBuild(
-        **pkginfo,
-        version=latest[0],
-        artifacts=[artifact],
-    )
-    ebuild.push()
+# vim: ts=4 sw=4 noet
