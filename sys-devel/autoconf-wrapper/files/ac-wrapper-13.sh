4bc5004dde5e8278d2a025aed9e05e24daa0ac3a
Daniel Robbins <drobbins@funtoo.org>
2024-03-14 14:32:25 +0000

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: 0a1a92ad3b868855780987ad10e422a85abe0e4c

--- sys-devel/autoconf-wrapper/files/ac-wrapper-13.sh
+++ sys-devel/autoconf-wrapper/files/ac-wrapper-13.sh
@@ -78,7 +78,7 @@ fi
 # Set up bindings between actual version and WANT_AUTOCONF;
 # Start at last known unstable/stable versions to speed up lookup process.
 #
-KNOWN_AUTOCONF="2.69:2.5 2.68:2.5"
+KNOWN_AUTOCONF="2.72:latest 2.69:2.5 2.68:2.5"
 vers="${KNOWN_AUTOCONF} 9999:2.5 $(printf '2.%s:2.5 ' `seq 99 -1 59`) 2.13:2.1"
 
 binary=""
