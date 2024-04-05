0022649bae4807563fcaa5cc90a4d305097849e2
Daniel Robbins <drobbins@funtoo.org>
2024-04-05 00:32:04 +0000

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: 1bfaf16ca5b4101471869c8a07e8d2e28e09c878

--- app-emulation/incus/autogen.py
+++ app-emulation/incus/autogen.py
@@ -3,8 +3,8 @@ from packaging.version import Version
 
 async def generate(hub, **pkginfo):
 	supported_releases = {
-		'latest': '>0.3.0',
-		'0.3.0': '==0.3.0',
+		'lts': '>=6.0.0',
+		'feature': '<1.0.0',
 	}
 	github_user = "lxc"
 	github_repo = "incus"
